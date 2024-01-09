0eadd15762abfa979c245290a79176b91e404f69
pmscarandang <155621046+pmscarandang@users.noreply.github.com>
2024-01-09 16:11:05 +0800

    Add files via upload

---
+++ KlondikeSolitaire/src/KlondikeSolitaire/Main.java
@@ -0,0 +1,15 @@
+package KlondikeSolitaire;
+/**
+ * This is the main class of the klondike solitaire
+ * @author pmcarandang
+ * 
+ */
+public class Main {
+	public static void main(String[] args) {
+		Game start = new Game();
+		start.initialize(); // initialize the foundations, tableau, talon, and waste pile
+		start.askForShuffle(); // method to ask player for shuffling the card before starting the game
+		start.displayInitialArrangement(); // display initial arrangement before starting the game
+		start.playGame(); // method for the game proper
+    }
+}
